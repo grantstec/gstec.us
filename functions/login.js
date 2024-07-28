8a53968ac16eb29e1e3be51a7f45f4cb155e1292
Grant <stecgrant89@gmail.com>
2024-07-28 11:55:14 -0600

    test

--- functions/login.js
+++ functions/login.js
@@ -6,6 +6,7 @@ export async function onRequestPost(context) {
     SELECT * FROM users WHERE username = ?
   `;
   const params = [username];
+  sessionStorage.setItem('username', username);
 
   try {
     const result = await bhdb.prepare(query).bind(...params).first();
